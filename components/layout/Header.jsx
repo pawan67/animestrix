be50ae24fc6952b7fc145b25d5bc21456240fdf6
Pawan <pawantamada8@gmail.com>
2022-12-10 19:37:08 +0530

    animation in menu

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -5,7 +5,7 @@ import { HiMenuAlt4 } from "react-icons/hi";
 import { IoClose } from "react-icons/io5";
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
+  const randomNum = Math.floor(Math.random() * 6) + 1;
   return (
     <>
       <div className=" p-5 px-5 sm:px-10 border-b border-primary-light bg-background/90 backdrop-blur-sm z-50  fixed flex justify-between left-0 items-center top-0 right-0  ">
@@ -25,11 +25,17 @@ function Header() {
       </div>
 
       <div
-        className={` bg-primary backdrop-blur-md transition-all pt-24 p-5 duration-1000   z-20 fixed h-screen ${
+        className={` bg-primary/90 backdrop-blur-md transition-all pt-24 p-5 duration-500    z-20 fixed h-[100vh] rounded-b-xl  ${
           isMenuOpen ? "top-0" : "-top-full"
         }  w-screen `}
       >
         <SearchInput />
+
+        <img
+          className="  w-full sm:hidden  mt-5"
+          src={`/images/loading/${randomNum}.gif`}
+          alt=""
+        />
       </div>
     </>
   );
